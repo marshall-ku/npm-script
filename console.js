19a4dd162136c303b4f3cc34be0d5b3533e6e7a7
marshall-ku <72745119+marshall-ku@users.noreply.github.com>
2024-06-10 10:20:59 +0900

    Test

---
+++ console.js
@@ -0,0 +1,10 @@
+console.log(`| Component | Story | Success | Viewport | MisMatch Percentage |
+| - | - | - | - | - |
+| test-typography-typography | default | ❌ | tablet | 100%(no reference) |
+| test-typography-typography | default | ❌ | phone | 100%(no reference) |
+| test-components-button | string-children | ❌ | tablet | 100%(no reference) |
+| test-components-button | string-children | ❌ | phone | 100%(no reference) |
+| components-button | string-children | ✅ | phone | 0.00% |
+| components-button | string-children | ✅ | tablet | 0.09% |
+| typography-typography | default | ✅ | phone | 0.00% |
+| typography-typography | default | ✅ | tablet | 0.00% |`)
