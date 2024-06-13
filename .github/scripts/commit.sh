39b99a1f2fdf094b4e82c23a2770bc722a9e974f
marshall-ku <72745119+marshall-ku@users.noreply.github.com>
2024-06-13 14:30:42 +0900

    F

--- .github/scripts/commit.sh
+++ .github/scripts/commit.sh
@@ -10,4 +10,7 @@ git config user.name github-actions[bot]
 git config user.email 41898282+github-actions[bot]@users.noreply.github.com
 git add "$files_to_commit"
 git commit -m "$MESSAGE"
-git push
+
+if [[ -z $PREVENT_PUSH ]]; then
+    git push
+fi
